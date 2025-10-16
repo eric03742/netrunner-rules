5ac1a69b9d2672e21e08958564f9295cd6cba75e
Eric03742 <eric03742@foxmail.com>
2025-10-16 14:37:50 +0800

    ✨ 将规则文本由 YAML 转换为 ParaTranz 使用的 CSV 格式.

---
+++ transformer/to_ParaTranz.py
@@ -0,0 +1,91 @@
+from typing import Any
+
+import csv
+import os
+import yaml
+
+
+def write_line(collector: list[list[str]], k: str, v: str):
+    line = list()
+    line.append(k)
+    line.append(v)
+    line.append("")
+    line.append("")
+    collector.append(line)
+
+
+def parse_content(collector: list[list[str]], content: dict[str, Any], index: int):
+    parse_chapter(collector, content, f"{index}.")
+
+
+def parse_chapter(collector: list[list[str]], content: dict[str, Any], index: str):
+    identifier = content["chapter"]
+    text = content["text"]
+    write_line(collector, identifier, f"{index} {text}")
+    sections = content["sections"]
+    for sub_index, section in enumerate(sections, start=1):
+        parse_section(collector, section, f"{index}{sub_index}.")
+
+
+def parse_section(collector: list[list[str]], content: dict[str, Any], index: str):
+    identifier = content["section"]
+    text = content["text"]
+    write_line(collector, identifier, f"{index} {text}")
+    if "snippet" in content:
+        snippet = content["snippet"]
+        write_line(collector, f"{identifier}-snippet", snippet)
+
+    rules = content["rules"]
+    for sub_index, rule in enumerate(rules, start=1):
+        parse_rule(collector, rule, f"{index}{sub_index}.")
+
+
+def parse_rule(collector: list[list[str]], content: dict[str, Any], index: str):
+    if "rule" in content:
+        identifier = content["rule"]
+        text = content["text"]
+        write_line(collector, identifier, f"{index} {text}")
+        # if "examples" in content:
+        #     examples = content["examples"]
+        #     for order, example in enumerate(examples, start=1):
+        #         parse_example(collector, example, identifier, order)
+    elif "subsection" in content:
+        identifier = content["subsection"]
+        text = content["text"]
+        write_line(collector, identifier, f"{index} {text}")
+        # if "examples" in content:
+        #     examples = content["examples"]
+        #     for index, example in enumerate(examples):
+        #         parse_example(example, content["subsection"], index, collector)
+
+        rules = content["rules"]
+        for sub_index, rule in enumerate(rules, start=1):
+            parse_rule(collector, rule, f"{index}{chr(ord('a') + sub_index - 1)}.")
+    elif "timing_structure" in content:
+        pass
+
+
+def parse_example(collector: list[list[str]], content: dict[str, Any], identifier: str, order: int):
+    text = content["text"]
+    write_line(collector, f"{identifier}-{order}-example", text)
+
+
+def main():
+    src_folder = "../data/input/"
+    dst_folder = "./output/"
+    filenames = sorted(os.listdir(src_folder))
+    for index, filename in enumerate(filenames, start=1):
+        collector = list()
+        with open(os.path.join(src_folder, filename), "r") as file:
+            content = yaml.safe_load(file)
+            parse_content(collector, content, index)
+
+        root, ext = os.path.splitext(filename)
+        with open(os.path.join(dst_folder, root + ".csv"), "w", newline="") as file:
+            writer = csv.writer(file)
+            for line in collector:
+                writer.writerow(line)
+
+
+if __name__ == "__main__":
+    main()
